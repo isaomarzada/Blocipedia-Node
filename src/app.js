18785e074b6d6b227765e99ee33e02bd3ebff727
Isa Omarzada <isaomarzada@yahoo.com>
2019-05-17 15:04:42 -0500

    heroku error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
-
+const appConfig = require("./config/main-config.js");
+app.use("/css",express.static(__dirname + "/css"));
+appConfig.init(app,express);
 routeConfig.init(app, express);
 
 module.exports = app;
