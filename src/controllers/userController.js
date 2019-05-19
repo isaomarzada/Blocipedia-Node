fdaf74773a3967a0700a56f728df53b9c8a020d5
Isa Omarzada <isaomarzada@yahoo.com>
2019-05-18 21:57:54 -0500

    sendgrid

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -16,6 +16,7 @@ module.exports = {
 
        userQueries.createUser(newUser, (err, user) => {
            if(err){
+               console.log("ERROR:", err);
                req.flash("error", err);
                res.redirect("/users/signup");
            }   else {
