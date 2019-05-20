eecc444ad9dc84eacd173b7a78f624c756aca601
Isa Omarzada <isaomarzada@yahoo.com>
2019-05-19 22:02:05 -0500

    User sign in complete

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,5 +6,7 @@ const validation = require("./validation");
 
 router.get("/users/signup", userController.signUp);
 router.post("/users", validation.validateUsers, userController.create);
-
+router.get("/users/sign_in", userController.signInForm);
+router.post("/users/sign_in", validation.validateUsers, userController.signIn);
+router.get("/users/sign_out", userController.signOut);
 module.exports = router;
