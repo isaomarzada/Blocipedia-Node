c4a308c80224dfc92eb3de7ea99bce03d962f52c
Isa Omarzada <isaomarzada@yahoo.com>
2019-05-20 23:31:00 -0500

    Wiki CRUD complete

---
+++ spec/integration/wikis_spec.js
@@ -0,0 +1,166 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/wikis/";
+const sequelize = require("../../src/db/models/index").sequelize;
+const Wiki = require("../../src/db/models").Wiki;
+const User = require("../../src/db/models").User;
+
+describe("routes : wikis ", () => {
+
+    beforeEach((done) => {
+        this.user;
+        this.wiki;
+        sequelize.sync({force: true}).then((res) => {
+
+            User.create({
+                email: "user@test.com",
+                password: "123456789"
+            })
+            .then((user) => {
+                this.user = user;
+
+                Wiki.create({
+                    title: "NFL",
+                    body: "Adrian Peterson is the best back of all time.",
+                    userId: this.user.id
+                })
+                .then((wiki) => {
+                    this.wiki = wiki;
+                    done();
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+    });
+
+    describe("GET /wikis", () => {
+
+        it("should return a status code 200 and all wikis", (done) => {
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(err).toBeNull();
+                expect(body).toContain("Wikis");
+                expect(body).toContain("Snowboarding");
+                done();
+            });
+        });
+    });
+
+    describe("GET /wikis/new", () => {
+
+        it("should render a new wiki form", (done) => {
+            request.get(`${base}new`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("New Wiki");
+                done();
+            });
+        });
+    });
+
+    describe("POST /wikis/create", () => {
+
+        it("should create a new wiki and redirect", (done) => {
+
+        const options = {
+            url: `${base}create`,
+            form: {
+                title: "NBA",
+                body: "Steph Curry is the best player to ever hold a basketball.",
+                userId: this.user.id,
+                private: false
+           },
+        };
+
+        request.post(options,
+              (err, res, body) => {
+                Wiki.findOne({where: {title: "NBA"}})
+                .then((wiki) => {
+                    expect(wiki.title).toBe("NBA");
+                    expect(wiki.body).toBe("Steph Curry is the best player to ever hold a basketball.");
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                });
+            }
+          );
+        });
+    });
+
+    describe("GET /wikis/:id", () => {
+
+        it("should render a view with the selected wiki", (done) => {
+            request.get(`${base}${this.wiki.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("NFL");
+                done();
+            });
+        });
+    });
+});
+describe("POST /wikis/:id/destroy", () => {
+
+       it("should delete the topic with the associated ID", (done) => {
+
+           Wiki.all()
+           .then((wikis) => {
+
+               const wikiCountBeforeDelete = wikis.length;
+
+               expect(wikiCountBeforeDelete).toBe(1);
+
+               request.post(`${base}${this.wiki.id}/destroy`, (err, res, body) => {
+                   Wiki.all()
+                   .then((wikis) => {
+                       expect(err).toBeNull();
+                       expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
+                       done();
+                   })
+               });
+           });
+       });
+   });
+
+   describe("GET /wikis/:id/edit", () => {
+
+       it("should render a view iwth an edit wiki form", (done) => {
+           request.get(`${base}${this.wiki.id}/edit`, (err, res, body) => {
+               expect(err).toBeNull();
+               expect(body).toContain("Edit Wiki");
+               expect(body).toContain("Snowboarding");
+               done();
+           });
+       });
+   });
+
+   describe("POST /wikis/:id/update", () => {
+
+       it("should update the wiki with the given values", (done) => {
+
+           const options = {
+               url: `${base}${this.wiki.id}/update`,
+               form: {
+                   title: "Surfing",
+                   body: "get so pitted!"
+               }
+           };
+
+           request.post(options,
+               (err, res, body) => {
+
+                   expect(err).toBeNull();
+
+                   Wiki.findOne({
+                       where: { id: this.wiki.id }
+                   })
+                   .then((wiki) => {
+                       expect(wiki.title).toBe("Football");
+                       done();
+                   });
+               });
+       });
+   });
