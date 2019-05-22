fc4570363cab0405219297e0d555f8f4a954e730
Isa Omarzada <isaomarzada@yahoo.com>
2019-05-21 21:47:04 -0500

    User Roles complete

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    role: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     defaultValue: 0
     }
   }, {});
   User.associate = function(models) {
@@ -19,6 +24,17 @@ module.exports = (sequelize, DataTypes) => {
    foreignKey: "userId",
    as: "wikis"
    });
+  };
+  User.prototype.isStandard = function() {
+    return this.role === 0;
+  };
+
+  User.prototype.isPremium = function() {
+    return this.role === 1;
+  };
+
+  User.prototype.isAdmin = function() {
+    return this.role === 2;
   };
   return User;
 };
